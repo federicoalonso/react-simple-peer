7d658886d60605469851f9aa85470b7c8567dad8
federicoalonso <f.nicolas.alonso@gmail.com>
2022-02-03 19:32:29 -0300

    Video Component

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    socket.emmit('me', socket.id);
+    socket.emit('me', socket.id);
 
     socket.on('disconnect', () => {
         socket.broadcast.emit('callended');
